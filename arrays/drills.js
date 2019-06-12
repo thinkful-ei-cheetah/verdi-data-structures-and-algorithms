83e41a7430a9778cef0ff6a08d1eb82476e0ddb8
Michael Verdi <michael.v.verdi@gmail.com>
2019-06-12 16:26:48 -0400

    merge solution

--- arrays/drills.js
+++ arrays/drills.js
@@ -57,6 +57,44 @@
 // }
 // let array1 = [5, 7, -3, 5, -2, 1]
 // console.log(findMaxSum(array1))
+// Solution is polynomial time of O(n^2)
+
+// Merge Arrays
+// Imagine you have 2 arrays which have already been sorted. Write an algorithm to merge the 2 arrays into a single array, which should also be sorted.
+
+// Input:[1, 3, 6, 8, 11] and [2, 3, 5, 8, 9, 10]
+// Output:[1, 2, 3, 3, 5, 6, 8, 8, 9, 10, 11]
 
 
+//[1, 3, 6, 8, 11] - [2, 3, 5, 8, 9, 10]
+//             -                        -
+//[1,2,3,3,5,6,8,8,9,10]
 
+function merge(arr1, arr2) {
+  let ptr1 = 0;
+  let ptr2 = 0;
+  let result = [];
+
+  while (arr1[ptr1] && arr2[ptr2]) {
+    if (arr1[ptr1] <= arr2[ptr2]) {
+      result.push(arr1[ptr1]);
+      ptr1++;
+    } else {
+      result.push(arr2[ptr2]);
+      ptr2++;
+    }
+  }
+
+  if (arr1[ptr1]) {
+    result = result.concat(arr1.slice(ptr1));
+  } else if (arr2[ptr2]) {
+    result = result.concat(arr2.slice(ptr2));
+  }
+
+  return result;
+}
+// const arr1 = [1, 3, 6, 8, 11];
+// const arr2 = [2, 3, 5, 8, 9, 10];
+// console.log(merge(arr1, arr2));
+
+// Big is Loglinear time O(nLogn)
