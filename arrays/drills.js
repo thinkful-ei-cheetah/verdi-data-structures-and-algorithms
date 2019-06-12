1e49339c289bd00d254abcf5b63cb6f25df24269
Michael Verdi <michael.v.verdi@gmail.com>
2019-06-12 14:41:27 -0400

    filter solution

--- arrays/drills.js
+++ arrays/drills.js
@@ -1,3 +1,4 @@
+'use strict';
 // 5. URLify a string
 // A common mistake users make when they type in an URL is to put spaces between words or letters. A solution that developers can use to solve this problem is to replace any spaces with a %20. Write a method that takes in a string and replaces all its empty spaces with a %20. Your algorithm can only make 1 pass through the string. Examples of input and output for this problem can be
 
@@ -14,3 +15,23 @@
 
 // console.log(urlify('tauhida parveen'))
 // console.log(urlify('www.thinkful.com /tauh ida parv een'))
+
+// Question 6
+// Imagine you have an array of numbers. Write an algorithm to remove all numbers less than 5 from the array. DO NOT use Array's built-in .filter() method here; write the algorithm from scratch.
+
+function myFilter(arr, filter) {
+  const results = [];
+  for (let i=0; i<arr.length; i++) {
+    let num = arr[i];
+    if (filter(num)) {
+      results.push(num);
+    }
+  }
+  return results;
+}
+// const testOne = [1,2,4,9,99];
+// const results = myFilter(testOne, (num) => num < 5 );
+// console.log(results);
+
+
+
