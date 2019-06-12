dd3a449600dd8c3e3af4170295a245a5fcf639e0
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-06-12 12:51:21 -0700

    maxSum

--- arrays/drills.js
+++ arrays/drills.js
@@ -19,19 +19,44 @@
 // Question 6
 // Imagine you have an array of numbers. Write an algorithm to remove all numbers less than 5 from the array. DO NOT use Array's built-in .filter() method here; write the algorithm from scratch.
 
-function myFilter(arr, filter) {
-  const results = [];
-  for (let i=0; i<arr.length; i++) {
-    let num = arr[i];
-    if (filter(num)) {
-      results.push(num);
-    }
-  }
-  return results;
-}
+// function myFilter(arr, filter) {
+//   const results = [];
+//   for (let i=0; i<arr.length; i++) {
+//     let num = arr[i];
+//     if (filter(num)) {
+//       results.push(num);
+//     }
+//   }
+//   return results;
+// }
 // const testOne = [1,2,4,9,99];
 // const results = myFilter(testOne, (num) => num < 5 );
 // console.log(results);
+
+// 7. max sum in array
+// You are given an array containing positive and negative integers. Write an algorithm which will find the largest sum in a continuous sequence.
+
+// Input: [4, 6, -3, 5, -2, 1]
+// Output: 12
+
+// function findMaxSum(arr) {
+//   let sum = 0;
+//   let maxSum = 0;
+
+//   for (let k = 0; k < arr.length; k++) {
+//     sum = 0
+//     for (let i = k; i < arr.length; i++) {
+//       sum += arr[i]
+//       if (sum > maxSum) {
+//         maxSum = sum;
+//       }
+//     }
+//   }
+
+//   return maxSum
+// }
+// let array1 = [5, 7, -3, 5, -2, 1]
+// console.log(findMaxSum(array1))
 
 
 
