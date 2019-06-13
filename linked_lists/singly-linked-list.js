41906108313748a6a04595601dac32128f406a2e
Michael Verdi <michael.v.verdi@gmail.com>
2019-06-13 11:28:02 -0400

    LL boilerplate

--- linked_lists/singly-linked-list.js
+++ linked_lists/singly-linked-list.js
@@ -1,1 +1,39 @@
 'use strict';
+
+class _Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class SinglyLinked {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(value) {
+    this.head = new _Node(value, this.head);
+  }
+}
+
+const list = new SinglyLinked();
+list.insertFirst(5);
+list.insertFirst(10);
+display(list);
+
+function display(list) {
+  let results = '';
+  let currentNode = list.head;
+  if (currentNode.next === null) {
+    console.log(`${currentNode.value}->null`);
+    return;
+  }
+
+  while (currentNode.next !== null) {
+    results += `${currentNode.value}->`;
+    currentNode = currentNode.next;
+  }
+  results += `${currentNode.value}->null`;
+  console.log(results);
+}
