2a1c825b9a873d8b28fa3996c4b1eff1117e91a9
Michael Verdi <michael.v.verdi@gmail.com>
2019-06-13 12:38:02 -0400

    implemented insertBefore

--- linked_lists/singly-linked-list.js
+++ linked_lists/singly-linked-list.js
@@ -25,44 +25,67 @@ class SinglyLinked {
     currentNode.next = node;
   }
 
-  remove(item) { // 5 => 10 => 15  = 15
-    if(!this.head) return null 
+  remove(item) {
+    if(!this.head) return null;
     if (this.head.value === item) {
-      this.head = this.head.next
-      return 
+      this.head = this.head.next;
+      return; 
     }
-    let currNode = this.head
-    let prevNode = this.head
+    let currNode = this.head;
+    let prevNode = this.head;
 
     while((currNode !== null) && (currNode.value !== item)) {
-      prevNode = currNode
-      currNode = currNode.next
+      prevNode = currNode;
+      currNode = currNode.next;
     }
     if(currNode === null) {
-      console.log('Item not found')
-      return
+      console.log('Item not found');
+      return;
     }
-    prevNode.next = currNode.next
+    prevNode.next = currNode.next;
   }
 
   find(item) {
-    let currNode = this.head
-    let result = ''
+    let currNode = this.head;
+    let result = '';
 
     while((currNode.value !== item) && (currNode.next !== null) ) {
-      currNode = currNode.next
+      currNode = currNode.next;
     }
-    result = currNode.value !== item ? 'Item not found' : `Result: ${JSON.stringify(currNode)}`
-    console.log(result)
+    result = currNode.value !== item ? 'Item not found' : `Result: ${JSON.stringify(currNode)}`;
+    console.log(result);
+  }
+
+  insertBefore(item, nodeVal) {
+    const newNode = new _Node(item);
+
+    if (this.head.next === null) {
+      const temp = this.head;
+      newNode.next = temp;
+      this.head = newNode;
+      return;
+    }
+    
+    let currNode = this.head;
+    while(currNode.next !== null && currNode.next.value !== nodeVal) {
+      currNode = currNode.next;
+    }
+
+    const tail = currNode.next;
+    currNode.next = newNode;
+    newNode.next = tail;
   }
 }
 
+// Implement a function called insertBefore() in the class that inserts a new node before a given node containing a key.
+
 const list = new SinglyLinked();
 list.insertFirst(5);
-list.insertLast(10);
-list.insertLast(15);
-list.remove(15);
-list.find(10)
+// list.insertLast(10);
+// list.insertLast(15);
+// list.remove(15);
+// list.find(10);
+list.insertBefore(12, 5);
 display(list);
 
 function display(list) {
