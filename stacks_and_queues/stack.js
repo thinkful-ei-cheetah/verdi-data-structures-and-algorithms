ec78237dddb017ae101a85b408204d10561f133d
Michael Verdi <michael.v.verdi@gmail.com>
2019-06-14 11:40:56 -0400

    implemented Stack

--- stacks_and_queues/stack.js
+++ stacks_and_queues/stack.js
@@ -1,1 +1,35 @@
 'use strict';
+
+//create a stack class that has push and pop
+
+class _Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(value) {
+    this.top = new _Node(value, this.top);
+  }
+
+  pop() {
+    const topNode = this.top;
+    this.top = this.top.next;
+    return topNode.value;
+  }
+}
+
+const starTrek = new Stack();
+starTrek.push('spock');
+starTrek.push('bob');
+starTrek.push('jim');
+// console.log(JSON.stringify(starTrek));
+
+
+
